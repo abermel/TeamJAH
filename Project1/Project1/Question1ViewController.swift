d22221b8561b9f9fe84b48c203748e730c9093cb
Amanda Bermel <46977224+abermel@users.noreply.github.com>
2021-09-07 18:01:44 -0500

    Add files via upload

---
+++ Project1/Project1/Question1ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  Question1ViewController.swift
+//  Project1
+//
+//  Created by Amanda Bermel on 9/7/21.
+//
+
+import UIKit
+
+class Question1ViewController: UIViewController {
+
+    @IBOutlet weak var question1Label: UILabel!
+    @IBOutlet weak var Question1ImgView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
